a4f1a896c750bbdff1ae2e6cfed2da0add8639ea
wangtao <595517713@qq.com>
2021-02-23 23:08:15 +0800

    feat(spider): 处理request异常ConnectionError，重新再抓取一次

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ import sqlite3
 from bs4 import BeautifulSoup
 from pprint import PrettyPrinter
 
-
 headers_name = {
     "referer": "https://mall.jd.com/",
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36",
@@ -22,9 +21,42 @@ PRICE_URL = "https://p.3.cn/prices/mgets"
 BASIC_URL = "https://item.jd.com/"
 sql = "SELECT pid FROM products"
 
+
+def grab_product_info(pid: int):
+    url = BASIC_URL + str(pid) + '.html'
+    res = requests.get(url, headers=headers_name)
+    if res.status_code == 200:
+        soup = BeautifulSoup(res.text, 'lxml')
+        shop_name = soup.find(class_='J-hove-wrap').find(class_='name').text.strip()
+        product_name = soup.find(class_='sku-name').text.strip()
+        itermover = soup.find(class_='itemover-tip')
+
+        if shop_name is not None:
+            logger.info("pid: {:<16} shop name: {}".format(pid, shop_name))
+        else:
+            logger.warning("pid: {:<16} no shop name".format(pid))
+        if product_name is not None:
+            logger.info("pid: {:<16} product name: {}".format(pid, product_name))
+        else:
+            logger.warning("pid: {:<16} no product name".format(pid))
+        if itermover is not None:
+            logger.critical("pid: {:<16} {}".format(pid, itermover.text.strip()))
+
+    params = {"skuIds": pid}
+    res = requests.get(PRICE_URL, headers=headers_price, params=params)
+    if res.status_code == 200:
+        res = json.loads(res.text)[0]
+        price = res.get('p')
+        if price is not None:
+            logger.info("pid: {:<16} price: {}".format(pid, price))
+        else:
+            logger.error("pid: {:<16} no price".format(pid))
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="python JD spider")
-    parser.add_argument('-a', '--add', action='store_true', default=False, help='是否添加新的商品')
+    parser.add_argument('-a', '--add', action='store_true', default=False, help='是否添加新商品')
+    parser.add_argument('-d', '--del', action='store_true', default=False, help='是否删除商品')
     parser.add_argument('-p', '--pids', type=int, default=None, nargs='+', help='商品id')
     parser.add_argument('-db', '--database', type=str, default=None, help='存放商品信息的数据库的路径')
     args = vars(parser.parse_args())
@@ -41,35 +73,19 @@ if __name__ == '__main__':
     elif args['pids'] is None and args['database'] is not None:
         conn = sqlite3.connect(args['database'])
         c = conn.cursor()
-        query_results =  c.execute(sql)
+        query_results = c.execute(sql)
         args['pids'] = [one[0] for one in query_results]
-
+    failed_pids = []
     for pid in args['pids']:
-        url = BASIC_URL + str(pid) + '.html'
-        res = requests.get(url, headers=headers_name)
-        if res.status_code == 200:
-            soup = BeautifulSoup(res.text, 'lxml')
-            shop_name = soup.find(class_='J-hove-wrap').find(class_='name').text.strip()
-            product_name = soup.find(class_='sku-name').text.strip()
-            itermover = soup.find(class_='itemover-tip')
-
-            if shop_name is not None:
-                logger.info("pid: {:<16} shop name: {}".format(pid, shop_name))
-            else:
-                logger.warning("pid: {:<16} no shop name".format(pid))
-            if product_name is not None:
-                logger.info("pid: {:<16} product name: {}".format(pid, product_name))
-            else:
-                logger.warning("pid: {:<16} no product name".format(pid))
-            if itermover is not None:
-                logger.critical("pid: {:<16} {}".format(pid, itermover.text.strip()))
-
-        params = {"skuIds": pid}
-        res = requests.get(PRICE_URL, headers=headers_price, params=params)
-        if res.status_code == 200:
-            res = json.loads(res.text)[0]
-            price = res.get('p')
-            if price is not None:
-                logger.info("pid: {:<16} price: {}".format(pid, price))
-            else:
-                logger.error("pid: {:<16} no price".format(pid))
+        try:
+            grab_product_info(pid)
+        except requests.exceptions.ConnectionError as e:
+            failed_pids.append(pid)
+            logger.error("pid: {:<16} {}".format(pid, str(e)))
+    if len(failed_pids) > 0:
+        logger.warning("grab failed products again")
+    for pid in failed_pids:
+        try:
+            grab_product_info(pid)
+        except requests.exceptions.ConnectionError as e:
+            logger.error("pid: {:<16} {}".format(pid, str(e)))
